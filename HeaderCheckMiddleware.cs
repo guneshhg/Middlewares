5dcd1792cc1112811d69fb34f858c0e56d464921
Hasan Hüseyin GÜNEŞ <guneshhg@outlook.com>
2021-10-21 22:53:59 +0300

    middlewares

---
+++ HeaderCheckMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middlewares
+{
+    public class HeaderCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HeaderCheckMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var key1 = httpContext.Request.Headers.Keys.Contains("Client-Key");
+            var key2 = httpContext.Request.Headers.Keys.Contains("Device-Id");
+            if(!key1 || !key2)
+            {
+                httpContext.Response.StatusCode = 400;
+                await httpContext.Response.WriteAsync("Missing required keys!");
+                return;
+            }
+            else
+            {
+
+            }
+            await _next.Invoke(httpContext);
+
+
+        }
+
+
+    }
+}
---
+++ MiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middlewares
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseHeaderCheckMiddleware(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<HeaderCheckMiddleware>();
+        }
+    }
+}
